b6a118e646255f98b3d430c70044a4d93f02d6d5
Omarabdullah99 <omarabdullah917303@gmail.com>
2023-12-19 20:11:27 +0600

    geAllCourses-getCourseBId-update&delete

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,5 +1,6 @@
 import Course from '../models/courseModel.js'
 
+//*create course
 export const createCourse= async(req,res)=>{
     const newCourse= new Course(req.body)
     try {
@@ -15,3 +16,75 @@ export const createCourse= async(req,res)=>{
         
     }
 }
+
+//*gel All Courses
+export const getAllCourses = async (req, res) => {
+    try {
+      const courses = await Course.find();
+      res.status(200).json({
+        success: true,
+        data: courses
+      });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  };
+
+  //*get CourseById
+  export const getCourseById = async (req, res) => {
+    const courseId = req.params.id;
+  
+    try {
+      const course = await Course.findById(courseId);
+      if (!course) {
+        res.status(404).send({
+            success: false,
+            message: 'Course not found',
+          })
+      }
+  
+      res.status(200).json({
+        success: true,
+        data: course
+      });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  };
+
+  //*updateCourse
+  export const updateCourse = async (req, res) => {
+    const courseId = req.params.id;
+    const updateData = req.body;
+    console.log('update',courseId)
+    console.log('data',updateData)
+  
+    try {
+      const updatedCourse = await Course.findByIdAndUpdate(courseId, updateData, { new: true });
+  
+      if (!updatedCourse) {
+        return res.status(404).json({ success: false, message: 'Course not found' });
+      }
+  
+      res.status(200).json({ success: true, data: updatedCourse });
+    } catch (error) {
+      res.status(500).json({ success: false, error: error.message });
+    }
+  };
+
+//*deleteCourse
+  export const deleteCourse = async (req, res) => {
+    const courseId = req.params.id;
+  
+    try {
+      const deletedCourse = await Course.findByIdAndDelete(courseId);
+  
+      if (!deletedCourse) {
+        return res.status(404).json({ success: false, message: 'Course not found' });
+      }
+  
+      res.status(200).json({ success: true, message: 'Course deleted successfully', data: deletedCourse });
+    } catch (error) {
+      res.status(500).json({ success: false, error: error.message });
+    }
+  };
--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -1,8 +1,14 @@
 import express from 'express'
 import { verifyAdmin } from '../utils/middleware.js'
-import { createCourse } from '../controllers/courseController.js'
+import { createCourse, deleteCourse, getAllCourses, getCourseById, updateCourse } from '../controllers/courseController.js'
 
 const router=express.Router()
+
+
+router.get("/getAllCourse",getAllCourses)
+router.get("/getCourse/:id",getCourseById)
+router.patch("/updateCourse/:id",verifyAdmin, updateCourse)
+router.delete("/delete/:id",verifyAdmin, deleteCourse)
 
 router.post("/createCourse",verifyAdmin, createCourse)
 
